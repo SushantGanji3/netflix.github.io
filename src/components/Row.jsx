bfeffca9bcccbad648747fde120b2c3f80334461
SushantGanji3 <sushantganji17@gmail.com>
2023-04-02 13:51:16 -0500

    first commit

---
+++ src/components/Row.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { FaHeart , FaRegHeart} from 'react-icons/fa'
+
+const Row = ({title, fetchURL}) => {
+    const [movies, setMovies] = useState([]);
+    const [like, setLike] = useState(false);
+
+    useEffect(() => {
+        axios.get(fetchURL).then((response) => {
+            setMovies(response.data.results);
+        });
+    }, [fetchURL]);
+    console.log(movies);
+
+  return (
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4'>
+        {title}
+      </h2>
+      <div className='relative flex items-center'>
+        <div id={'slider'}>
+            {movies.map((item, id) => (
+                <div className='w-[160px] sm:w-[200px] md: x-[240px] lg:x-[280px] inline-block cursor-pointer relative p-4'>
+                    <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.id}/>
+                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                        <p className='white-space-normal text-xs md:text-sm text-bold flex justify-center items-center h-full text-center'>{item.title}</p>
+                        <p>
+                            {like ? <FaHeart /> : <FaRegHeart />}
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Row
