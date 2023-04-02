fdf8856d07fce3ccc6a12c473899a09ba10980ca
SushantGanji3 <sushantganji17@gmail.com>
2023-04-02 15:47:43 -0500

    Home Page Down!

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import Movies from './Movies';
+import axios from 'axios'
+import Movies from './Movies'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-const Row = ({title, fetchURL}) => {
+
+const Row = ({ rowId, title, fetchURL}) => {
     const [movies, setMovies] = useState([]);
     
 
@@ -11,19 +13,29 @@ const Row = ({title, fetchURL}) => {
             setMovies(response.data.results);
         });
     }, [fetchURL]);
-    console.log(movies);
+    
+    const slideLeft = () => {
+        var slider = document.getElementById('slider' + rowId);
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+    const slideRight = () => {
+        var slider = document.getElementById('slider' + rowId);
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
 
   return (
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>
         {title}
       </h2>
-      <div className='relative flex items-center'>
-        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+      <div className='relative flex items-center group'>
+        <MdChevronLeft onClick={slideLeft} className='bg-white left-0 rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+        <div id={'slider' + rowId} className='relative flex w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
             {movies.map((item, id) => (
                 <Movies key={id} item={item}/>
             ))}
         </div>
+        <MdChevronRight onClick={slideRight} className='bg-white right-0 rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
       </div>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
-import Row from '../components/Row'
+import Row from '../components/Row' 
 import requests from '../Requests'
 
 const Home = () => {
   return (
     <div>
       <Main />
-      <Row title="Upcoming" fetchURL={requests.requestUpcoming}/>
-      <Row title="Popular" fetchURL={requests.requestPopular}/>
-      <Row title="TopRated" fetchURL={requests.requestTopRated}/>
-      <Row title="Trending" fetchURL={requests.requestTrending}/>
-      <Row title="Horror " fetchURL={requests.requestHorror}/>
+      <Row rowId="1" title="Upcoming" fetchURL={requests.requestUpcoming}/>
+      <Row rowId="2" title="Popular" fetchURL={requests.requestPopular}/>
+      <Row rowId="3" title="TopRated" fetchURL={requests.requestTopRated}/>
+      <Row rowId="4" title="Trending" fetchURL={requests.requestTrending}/>
+      <Row rowId="5" title="Horror" fetchURL={requests.requestHorror}/>
     </div>
   )
 }
