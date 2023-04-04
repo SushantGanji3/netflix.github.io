ddfb4ed60fdb286fa4292452d798e11e4693baf5
SushantGanji3 <sushantganji17@gmail.com>
2023-04-04 16:19:46 -0500

    4th commit

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -4,6 +4,7 @@ import { UserAuth } from '../context/AuthContext'
 import { db } from '../firebase'
 import {updateDoc, doc, onSnapshot} from 'firebase/firestore'
 import {AiOutlineClose} from 'react-icons/ai'
+import { async } from '@firebase/util'
 
 const SavedShows = () => {
     const [movies, setMovies] = useState([]);
@@ -24,6 +25,17 @@ const SavedShows = () => {
         })
     }, [user?.email]);
 
+    const movieRef = doc(db, 'users', `${user?.email}`);
+    const deleteShow = async (passedID) => {
+      try {
+        const result = movies.filter((item) => item.id !== passedID);
+        await updateDoc(movieRef, {
+          savedShows: result,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
   return (
     <div>
       <h2 className='text-white font-bold md:text-xl p-4'>
@@ -39,7 +51,7 @@ const SavedShows = () => {
                   <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                       
                     <p className='white-space-normal text-xs md:text-sm text-bold flex justify-center items-center h-full text-center'>{item.title}</p>
-                    <p className='absolute text-gray-300 top-4 right-4'><AiOutlineClose /></p>
+                    <p onClick={() => deleteShow(item.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose /></p>
                   </div>
               </div>
               </div>
